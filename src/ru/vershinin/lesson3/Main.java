0bb1b3594e490ff0d64625cbd456c4b459d7b828
Вершинин Пётр <pitver2014@yandex.ru>
2020-05-05 20:19:31 +0300

    Lesson3-fix_05.05.2020

--- src/ru/vershinin/lesson3/Main.java
+++ src/ru/vershinin/lesson3/Main.java
@@ -12,15 +12,15 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-       Integer[] arrNum=new Integer[25];
+        Integer[] arrNum = new Integer[25];
         Random rnd = new Random();
-        for (int i = 0; i <arrNum.length ; i++) {
-            arrNum[i]=rnd.nextInt(60);
+        for (int i = 0; i < arrNum.length; i++) {
+            arrNum[i] = rnd.nextInt(60);
         }
-        MathBox mb= new MathBox(arrNum);
+        MathBox mb = new MathBox(arrNum);
         mb.dump();
-        Number res =mb.summator(mb);
-        System.out.println("сумма всех элементов коллекции = "+res);
+        Number res = mb.summator();
+        System.out.println("сумма всех элементов коллекции = " + res);
         mb.splitter(2);
         mb.dump();
         mb.deleteObject(4);
