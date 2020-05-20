b07247659cd5326b1f1b3c181899f8717ef3dbcb
Вершинин Пётр <pitver2014@yandex.ru>
2020-05-20 11:37:39 +0300

    Lesson7-20.05.2020 распараллеливать вычисления для разных чисел

--- src/ru/vershinin/lesson7/GetSortArray.java
+++ src/ru/vershinin/lesson7/GetSortArray.java
@@ -1,27 +1,34 @@
 package ru.vershinin.lesson7;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class GetSortArray {
-    protected static int[] getNumbers(Set<Integer> tempNum, int n) {
 
-        for(int i = 0; i < n; i++) {
+    /**
+     * формирует массив отсортированных элементов
+     *
+     * @param countNumbers - длина массива
+     * @return - возвращаем отсортированный массив
+     */
+    protected static int[] getNumbers( int countNumbers) {
+        Set<Integer> tempNum = new HashSet<>();
+        for(int i = 0; i < countNumbers; i++) {
             Random rd=new Random();
-            int num=rd.nextInt(10);
-            if(num>0){
+            int num=rd.nextInt(50);
+            if(num>1){
                 tempNum.add(num);
-
             }
         }
         int[] arr = new int[tempNum.size()];
-        int i = 0;
-        for (int x : tempNum)
+        int indexArr = 0;
+        for (int countTempNum : tempNum)
         {
-            System.out.print(x);
+            System.out.print(countTempNum);
             System.out.print(" ");
-            arr[i++] = x;
+            arr[indexArr++] = countTempNum;
 
         }
         Arrays.sort(arr);
--- src/ru/vershinin/lesson7/Main.java
+++ src/ru/vershinin/lesson7/Main.java
@@ -7,122 +7,82 @@ import java.util.concurrent.*;
 public class Main {
 
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Set<Integer> tempNum = new HashSet<>();
-        int[] arr = GetSortArray.getNumbers(tempNum, 5);
+    /**
+     * вычисления факторила путем
+     * распараллеливания вычисления для разных чисел
+     * с запоминания факториал для одного числа и использованием
+     * для выесдения другого
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int[] arr = GetSortArray.getNumbers(5);// получаем отсортированный массив
         ExecutorService executor = Executors.newFixedThreadPool(1);
-
         List<Future<BigInteger>> resultList = new ArrayList<>();
-        FactorialCalculator calculator = null;
-
+        FactorialCalculator calculator;
+        // циклом проходимся по всему массиву
         for (int a = 0; a < arr.length; a++) {
             calculator = new FactorialCalculator(arr[a]);
             Future<BigInteger> result = executor.submit(calculator);
             resultList.add(result);
         }
-/*        for (Future<BigInteger> res : resultList) {
-            BigInteger num = res.get();
-            System.out.println(num);
-        }*/
 
         try {
-            executor.awaitTermination(2, TimeUnit.SECONDS);
+            executor.awaitTermination(1, TimeUnit.SECONDS);// блокируем на 1 сек
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        executor.shutdown();
+        executor.shutdown();//закрываем
     }
 
 
 }
 
 class FactorialCalculator implements Callable<BigInteger> {
+
     public static Map<Integer, BigInteger> factoryMap = new ConcurrentHashMap<>();
     private final Integer number;
-    private int count = 1;
-    BigInteger resultCalc = new BigInteger("1");
+    private static int count = 1;
+    static BigInteger resultCalc = new BigInteger("1");
+
+
     public FactorialCalculator(Integer number) {
         this.number = number;
-
     }
 
-
+    /**
+     * для эфективного вычисления сохраняем предъидущий результат факториала
+     * в потокобезопасную коллекцию ConcurrentHashMap<>(), где key- это число из массива, value- факториал этого числа,
+     * и при вычичлении нового значения факториала используем уже вычесленный
+     *
+     * @return возвращает значение факториала, тип Future<BigInteger>
+     * @throws Exception
+     */
     @Override
     public BigInteger call() throws Exception {
-       // Integer maxKey = Collections.max(factoryMap.keySet());
-
+           // получаем ключ и значения факториала
         Iterator iterator = factoryMap.keySet().iterator();
         while (iterator.hasNext()) {
             Integer key = (Integer) iterator.next();
             if (key < number) {
-                count=key;
-            }else {
-                return factoryMap.get(number);
+                count = key;
+                resultCalc = factoryMap.get(count);
             }
         }
 
 
+        count++;//чтобы избежать повторного использования предъидущего значения ключа, увеличиваем его на 1
         for (int i = count; i <= number; i++) {
-            resultCalc = (factoryMap.get(count)).multiply(new BigInteger(i + ""));
+            resultCalc = resultCalc.multiply(new BigInteger(i + ""));
             factoryMap.put(i, resultCalc);
-
         }
+
         System.out.printf("Factorial of %d is :: %d\n", number, factoryMap.get(number));
         return resultCalc;
     }
 }
 
-/*
-            Result result = new Result(arr[0], 0, arr.length / 2);
-
-            ExecutorService service = Executors.newFixedThreadPool(1);
-
-
-            Future<BigInteger> future = service.submit(result);
-
-            try {
-                String res = String.valueOf(future.get());
-                System.out.println(res);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-        }      // Result result1 = new Result(arr,arr.length/2,arr.length);
-
-
-
-
-
-
-    static class Result implements Callable<BigInteger> {
-        private int n;
-        private int a;
-        private int b;
-
-        public Result(int num,int a,int b) {
-            this.n = num;
-            this.a = a;
-            this.b = b;
-        }
-
-
-
-        *//**
- * Computes a result, or throws an exception if unable to do so.
- *
- * @return computed result
- * @throws Exception if unable to compute a result
- *//*
-        @Override
-        public BigInteger call() throws Exception {
-            BigInteger result = new BigInteger("1");
-            for (int i = a; i <=b ; a++) {
-                result=result.multiply(new BigInteger(i + ""));
-            }
-            return result;
-        }
-    }*/
 
 
 
