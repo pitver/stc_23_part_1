7f219d3d0cd3518cf1851ba60c2366300c8e7f49
pitver <pitver2014@yandex.ru>
2020-05-23 23:55:08 +0300

    Lesson12

--- src/ru/vershinin/lesson12/Task2/Permgen.java
+++ src/ru/vershinin/lesson12/Task2/Permgen.java
@@ -5,8 +5,10 @@ import javassist.ClassPool;
 
 /**
  * генерирация новых классов и загрузка их в Permgen
+ * использовать -XX:MaxPermSize=128m
  *
- * @Exception - java.lang.OutOfMemoryError
+ * @Exception - java.lang.OutOfMemoryError PermGen указывает на то,
+ * \что область постоянного поколения в памяти исчерпана
  */
 public class Permgen {
     static ClassPool classPool = ClassPool.getDefault();
