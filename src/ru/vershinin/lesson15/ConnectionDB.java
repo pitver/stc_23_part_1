0fdb4086e1b219012663aa5db0b81165096fbc1d
Вершинин Пётр <pitver2014@yandex.ru>
2020-06-01 16:35:12 +0300

    Lesson15-01062020

---
+++ src/ru/vershinin/lesson15/ConnectionDB.java
@@ -0,0 +1,46 @@
+package ru.vershinin.lesson15;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * ChangeDB
+ *
+ * @author Вершинин Пётр
+ */
+public class ConnectionDB {
+
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String DB_Driver = "org.postgresql.Driver";
+    public static void connectClose() {
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL,
+                    "postgres", "root");//соединениесБД
+            conn.close();
+            System.out.println("Соединение с СУБД закрыто.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Connection connect() {
+        try {
+            Class.forName(DB_Driver); //Проверяем наличие JDBC драйвера для работы с БД
+            Connection connection = DriverManager.getConnection(DB_URL,
+                    "postgres", "root");//соединениесБД
+            System.out.println("Соединение с СУБД выполнено.");
+            return connection;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); // обработка ошибки  Class.forName
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
+        }
+        return null;
+    }
+
+}
---
+++ src/ru/vershinin/lesson15/Order.java
@@ -0,0 +1,24 @@
+package ru.vershinin.lesson15;
+
+/**
+ * Order
+ *
+ * @author Вершинин Пётр
+ */
+public class Order {
+    Client client;
+    Product product;
+
+    public Order(Client client, Product product) {
+        this.client = client;
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "client=" + client +
+                ", product=" + product +
+                '}';
+    }
+}
